13314cd72cabda2e7806342f9ad6d5da51969b9e
ZeroPowerZero <sharmasusheel904@gmail.com>
2026-01-26 23:26:04 +0530

    organsm satification push but still some issue which danish gonna look

--- scripts/state.gd
+++ scripts/state.gd
@@ -4,6 +4,7 @@ extends Node2D
 
 var dragging := false
 var drag_offset := Vector2.ZERO
+var last_mouse_pos = Vector2.ZERO
 
 func is_mouse_over() -> bool:
 	var mouse = to_local(get_global_mouse_position())
@@ -11,28 +12,29 @@ func is_mouse_over() -> bool:
 	return mouse.x > -40 and mouse.x < 40 and mouse.y > -40 and mouse.y < 40
 
 func _input(event):
-	print("hello")
-	#mouse button pressed on state
+	# LEFT MOUSE BUTTON
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		print("i am clicked")
-		
-			#check if click is on this state
-		if is_mouse_over():
+		if event.pressed:
+			if is_mouse_over():
+				print("mouse is over")
 				dragging = true
 				drag_offset = global_position - get_global_mouse_position()
-				
-				
-		else:
+				get_viewport().set_input_as_handled()
+			else:
 				dragging = false
-				
-				
-		if event is InputEventMouseMotion and dragging:
-			global_position = get_global_mouse_position() + drag_offset
-			
-	#get_viewport().set_input_as_handeled()
+		else:
+			# mouse released
+			dragging = false
+
+	# MOUSE DRAG
+	if event is InputEventMouseMotion and dragging:
+		global_position = get_global_mouse_position() + drag_offset
+		get_viewport().set_input_as_handled()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print(is_mouse_over())
 	$Label.text = state_name
 	get_tree().get_first_node_in_group("camera").block_pan = false
 	print(get_tree_string())
