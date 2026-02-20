615086f5075efac8b5313148d4d71095c01580d1
ZeroPowerZero <sharmasusheel904@gmail.com>
2026-02-21 00:53:00 +0530

    extra deleted

--- scripts/state.gd
+++ scripts/state.gd
@@ -1,21 +1,28 @@
 class_name State extends movement
+
 @export var state_name := "qo"
 const inputSymbols=["a","b","*"]
-var state
-var name
-var state_links:Dictionary[string,State]={}
 
-func setUp(name,states:Dictionary):
-	state_links=states
-	self.name=name	
-func sendTo(s:State):
-	s.state=state
-	
-func compute():
-	for pair in state_links:
-		var char = pair.
-		
-	
-
-func _process(delta: float) -> func:
+func _ready() -> void:
 	pass
+	
+
+#var state
+#var name
+#var state_links:Dictionary[string,State]={}
+
+#func setUp(name,states:Dictionary):
+	#state_links=states
+	#self.name=name	
+	#
+#func sendTo(s:State):
+	#s.state=state
+	#
+#func compute():
+	#for pair in state_links:
+		#var char = pair
+		#
+	#
+#
+#func _process(delta: float) -> void:
+	#pass
