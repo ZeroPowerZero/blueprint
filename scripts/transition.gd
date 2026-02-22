276d2638e8e81538f7ac5dbca917444f5e48c225
ZeroPowerZero <sharmasusheel904@gmail.com>
2026-02-22 23:43:48 +0530

    arrow direction added

--- scripts/transition.gd
+++ scripts/transition.gd
@@ -7,12 +7,10 @@ var symbol : String
 @onready var line = $Line2D
 @onready var label = $Label
 
-
 func setup(from_node, to_node, input_symbol):
 	from_state = from_node
 	to_state = to_node
 	symbol = input_symbol
-	
 	label.text = symbol
 
 
@@ -32,7 +30,7 @@ func update_curve():
 	var mid = (start + end) / 2
 	var direction = (end - start).normalized()
 	var normal = Vector2(-direction.y, direction.x)
-	var offset = normal * 80
+	var offset = normal * 10
 	
 	curve.add_point(start)
 	curve.add_point(mid + offset)
@@ -40,3 +38,24 @@ func update_curve():
 	
 	line.points = curve.get_baked_points()
 	label.position = mid + offset
+	label.scale = Vector2(2,2)
+	
+	queue_redraw()   # 🔥 needed for arrow
+
+
+func _draw():
+	if from_state == null or to_state == null:
+		return
+	
+	var end = to_state.position
+	var start = from_state.position
+	
+	var dir = (end - start).normalized()
+	
+	var arrow_size = 30
+	
+	var p1 = end
+	var p2 = end - dir * arrow_size + Vector2(-dir.y, dir.x) * (arrow_size / 2)
+	var p3 = end - dir * arrow_size + Vector2(dir.y, -dir.x) * (arrow_size / 2)
+	
+	draw_polygon([p1, p2, p3], [Color.DODGER_BLUE])
